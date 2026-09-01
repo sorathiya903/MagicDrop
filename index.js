1ba2cf44cebfd777e970397e040805a91cde4d9d
sorathiya903 <adityasorathiya.4112@gmail.com>
2026-09-01 12:09:16 +0530

    formatSize added

--- index.js
+++ index.js
@@ -27,6 +27,29 @@ if (!fs.existsSync(DOWNLOAD_DIR)) {
 // ==============================
 // LOCAL IP
 // ==============================
+function formatSize(bytes) {
+
+    if (!bytes || bytes <= 0)
+        return "0 B";
+
+    const units = [
+        "B",
+        "KB",
+        "MB",
+        "GB",
+        "TB"
+    ];
+
+    const index =
+        Math.floor(
+            Math.log(bytes) / Math.log(1024)
+        );
+
+    const value =
+        bytes / Math.pow(1024, index);
+
+    return `${value.toFixed(index === 0 ? 0 : 1)} ${units[index]}`;
+}
 function sendTerminalFileToReceiver(
     transfer,
     receiverId
